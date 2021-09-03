e7c11684de81ce3355cc34849275103c5055d25b
agreyson <amgreyson@gmail.com>
2021-09-03 14:47:49 -0700

    Added test.

--- 100DaysOfSwift.playground/Pages/Exercises.xcplaygroundpage/Contents.swift
+++ 100DaysOfSwift.playground/Pages/Exercises.xcplaygroundpage/Contents.swift
@@ -13,5 +13,6 @@ print("Seniors graduate in \(graduationYear(gradeLevel: GradeLevel.senior)).")
 print("Sixth graders graduate in \(graduationYear(gradeLevel: GradeLevel.sixth)).")
 print("Eighth graders graduate in \(graduationYear(gradeLevel: GradeLevel.eighth)).")
 print("Juniors graduate in \(graduationYear(gradeLevel: GradeLevel.junior)).")
+print("Eighth graders graduate in \(graduationYear(gradeLevel: GradeLevel.eighth)).")
 
 
