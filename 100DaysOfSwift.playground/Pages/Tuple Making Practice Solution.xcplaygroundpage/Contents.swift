ca4fb6ec4401abdbcbe9fda234ee8883eca6ce1f
23cdurkin <23cdurkin@castilleja.org>
2021-09-08 11:06:58 -0700

    :)

---
+++ 100DaysOfSwift.playground/Pages/Tuple Making Practice Solution.xcplaygroundpage/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+// create a tuple that keeps the information of three students (names(string), age (int), grade (int), attendence (boolean)) and print all of their names
+
+var student = (name: "Annie", age: 15, grade: 10, attendence: true)
+var student1 = (name: "Benny", age: 15, grade: 10, attendence: false)
+var student2 = (name: "Carlisle", age: 15, grade: 10, attendence: true)
+print (student.name + student1.name + student2.name)
---
+++ 100DaysOfSwift.playground/Pages/Tuple Making Practice.xcplaygroundpage/Contents.swift
@@ -0,0 +1,1 @@
+// create a tuple that keeps the information of three students (names(string), age (int), grade (int), attendence (boolean)) and print all of their names
